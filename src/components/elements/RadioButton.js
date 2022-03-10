6b50f5c616909cc2db4c1742103d03458f802967
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-03-10 16:31:51 +0800

    Update RadioButton.js

--- src/components/elements/RadioButton.js
+++ src/components/elements/RadioButton.js
@@ -2,18 +2,18 @@ import React from "react";
 
 const RadioButton = ({ id, label, value, group, disabled }) => {
   return (
-    <div class="flex items-center mb-4">
+    <div className="flex items-center mb-4">
       <input
         id={id}
         type="radio"
         name={group}
         value={value}
-        class="cursor-pointer w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
+        className="cursor-pointer w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
         disabled={disabled}
       />
       <label
-        for={id}
-        class="cursor-pointer block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        htmlFor={id}
+        className="cursor-pointer block ml-2 text-sm font-medium text-gray-900 hover:text-gray-500"
       >
         {label}
       </label>
